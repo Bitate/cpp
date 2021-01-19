1ed22c416a40f83805b911eab411a111ffa0b01b
Bitate <1660513875@qq.com>
2021-01-19 16:44:26 +0800

    Add static member definition issue
    * Static member variable should be defined
    in the source file instead of header file;
    * You can put declaration and definition of static
    member variables within the class {}; but that variable
    must be const/constexpr;
    * Static member functions can only access the static
    member variables of the same class;

--- basic/static_member_1.cpp
+++ basic/static_member_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -22,9 +23,19 @@ public:
 
 public:
 	static int object_count;
+
+	// you can put definition inside the class 
+	// but this member must be const!
+	static constexpr int maximum_capacity = 100; 
+
+	static vector<double> vec;
+	// static vector<double> vec(maximum_capacity);
+	//                            ^^^^^^^^^^^^^
+	// 					put definition outside of class :^)
 };
 
 int Point::object_count = 0;
+vector<double> Point::vec(Point::maximum_capacity);
 
 int sum()
 {
@@ -39,7 +50,7 @@ int main()
 	Point p2;
 	Point p3;
 	Point p4;
-
+	
 	printf("object_count is: %d\n", p1.get_count());
 
 	p1.~Point();
--- cpp-stls/test/unordered_set_test_1.cpp
+++ cpp-stls/test/unordered_set_test_1.cpp
@@ -1,4 +1,5 @@
 #include <unordered_set>
+#include <stdio.h>
 
 using namespace std;
 
